679456ca186d8deb79860cec0df44078e7c3487f
tiloubrunet <72348469+tiloubrunet@users.noreply.github.com>
2020-10-04 10:54:42 -0400

    Update test.s

--- test.s
+++ test.s
@@ -4,5 +4,5 @@ select
 	pick 'td:nth-child(3)' as wind,
 	pick 'td:nth-child(4)' as visibilty,
 	pick 'td:nth-child(5)' as weather
-from download page 'https://w1.weather.gov/data/obhistory/KGTU.html'
+from download page 'https://www.cdw.ca/product/apple-macbook-pro-with-touch-bar-13.3-core-i5-16-gb-ram-1-tb-ssd/6070106'
 where nodes = 'body table:nth-child(4) tr[valign=top]:nth-child(-n+10)'
