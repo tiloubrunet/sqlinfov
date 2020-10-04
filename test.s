60865a682e1813f18f45a93280bf64edf0b60f96
tiloubrunet <72348469+tiloubrunet@users.noreply.github.com>
2020-10-04 11:08:31 -0400

    Update test.s

--- test.s
+++ test.s
@@ -1,8 +1,8 @@
 select 
-	pick '.mpn',
+	pick '.a-size-medium a-color-price priceBlockBuyingPriceString' as Price,
 	pick 'td:nth-child(2)' as time,
 	pick 'td:nth-child(3)' as wind,
 	pick 'td:nth-child(4)' as visibilty,
 	pick 'td:nth-child(5)' as weather
-from download page 'https://www.cdw.ca/product/apple-macbook-pro-with-touch-bar-13.3-core-i5-16-gb-ram-1-tb-ssd/6070106'
+from download page 'https://www.amazon.ca/-/fr/gp/product/B06ZZVTD8M?pf_rd_r=GTE8K6F54T8CHWXRSP97&pf_rd_p=b52073b7-e00d-4632-b0c5-b58ce02b7a94'
 where nodes = 'body table:nth-child(4) tr[valign=top]:nth-child(-n+10)'
