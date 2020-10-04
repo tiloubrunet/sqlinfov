90e783c335b9449ed54bfb2f93188b8003c6abfa
tiloubrunet <72348469+tiloubrunet@users.noreply.github.com>
2020-10-04 12:15:34 -0400

    Update and rename amazon5.s to amazon6.s

--- amazon6.s
+++ amazon6.s
@@ -5,4 +5,4 @@ where nodes = 'table.a-lineitem tbody tr'
 
 pick 'td:nth-child(1)' as Brand
 from download page 'https://www.amazon.ca/-/fr/gp/product/B06ZZVTD8M?pf_rd_r=GTE8K6F54T8CHWXRSP97&pf_rd_p=b52073b7-e00d-4632-b0c5-b58ce02b7a94'
-where nodes = 'table.a-keyvalue prodDetTable tbody tr'
+where nodes = 'table.a-keyvalue.prodDetTable tbody tr'
