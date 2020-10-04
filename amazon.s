149690bdc80dea5665dba01489928ef869d562f8
tiloubrunet <72348469+tiloubrunet@users.noreply.github.com>
2020-10-04 11:53:18 -0400

    Update amazon.s

--- amazon.s
+++ amazon.s
@@ -1,3 +1,3 @@
 select 
-	pick 'div.price' as Price
+	pick 'td:nth-child(1)' as Price
 from download page 'https://www.amazon.ca/-/fr/gp/product/B06ZZVTD8M?pf_rd_r=GTE8K6F54T8CHWXRSP97&pf_rd_p=b52073b7-e00d-4632-b0c5-b58ce02b7a94'
